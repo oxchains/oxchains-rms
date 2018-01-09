63dd1aab57d7fc595b26258cfd1780d69a60abd3
cherrishccl <528050239@qq.com>
2018-01-09 13:03:15 +0800

    init code

---
+++ BackEnd/rms-user/src/main/java/com/oxchains/rmsuser/RmsUserApplication.java
@@ -0,0 +1,12 @@
+package com.oxchains.rmsuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmsUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmsUserApplication.class, args);
+	}
+}
